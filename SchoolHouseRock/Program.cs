1f65022e1dc420962accd60c0bec8c10c0eacd7b
nikolemc <nikolemcstanley@gmail.com>
2017-04-04 18:35:44 -0400

    Display the total number of courses, and the total number of instructors

--- SchoolHouseRock/Program.cs
+++ SchoolHouseRock/Program.cs
@@ -40,9 +40,55 @@ namespace SchoolHouseRock
             connection.Close();
             return courses;
         }
+
+        static public int GetTotalCourse(SqlConnection connection)
+        {
+            var totalcourses = (0);
+            // Basic Select
         
+            // estblish the QUERY
+            var sqlCommand = new SqlCommand(@"SELECT DISTINCT COUNT(CourseTitle) AS 'TOTALCOURSES'
+                                           FROM [dbo].Course", connection);
+            // open the connection
+            connection.Open();
+            // Run Query
+            var reader = sqlCommand.ExecuteReader();
+            // Read Results
+            while (reader.Read())
+            {
+                //var course = new Course(reader);
 
+                totalcourses = (int)reader["TOTALCOURSES"];
+            }
+            // Close Connection
+            connection.Close();
+            return totalcourses;
+        }
 
+        static public int GetTotalInstructors(SqlConnection connection)
+        {
+            var totalinstructors = (0);
+            // Basic Select
+
+            // estblish the QUERY
+            var sqlCommand = new SqlCommand(@"SELECT DISTINCT COUNT(InstructorName) AS 'TOTALINSTRUCTORS'
+                                           FROM [dbo].Instructor", connection);
+            // open the connection
+            connection.Open();
+            // Run Query
+            var reader = sqlCommand.ExecuteReader();
+            // Read Results
+            while (reader.Read())
+            {
+                //var course = new Course(reader);
+
+                totalinstructors = (int)reader["TOTALINSTRUCTORS"];
+            }
+            // Close Connection
+            connection.Close();
+            return totalinstructors;
+        }
+        
 
         static void Main(string[] args)
         {
@@ -55,15 +101,23 @@ namespace SchoolHouseRock
             using (var connection = new SqlConnection(connectionString))
             {
                 courses = GetAllCourses(connection);
-                Console.WriteLine("DotNet University");
+                Console.WriteLine("Courses offered at DotNet University:");
                 foreach (var course in courses)
                 {
                     Console.WriteLine(course.CourseTitle + ": Taught by " + course.InstructorName);
-                }
-            }
 
+                }
+
+                int totalcources = GetTotalCourse(connection);
+                Console.WriteLine($"\nThe total number of courses taught at DotNet University is: { totalcources}");
+                int totalinstructors = GetTotalInstructors(connection);
+                Console.WriteLine($"The total number of instructors this semester is: {totalinstructors}");
+            }
+            
+            
+
+
+        }
 
     }
-
-}
 }
