65e36b509e728b5af0887ef372ff15d7e4beb4b2
nikolemc <nikolemcstanley@gmail.com>
2017-04-04 15:49:51 -0400

    Setup connecting to my SQl Database

--- SchoolHouseRock/Program.cs
+++ SchoolHouseRock/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace SchoolHouseRock
     {
         static void Main(string[] args)
         {
+            // Where is the Database
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=SchoolData;Trusted_Connection=True;";
+            // Open with using something
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connected");
+            }
+
+
+
+
         }
+
     }
 }
